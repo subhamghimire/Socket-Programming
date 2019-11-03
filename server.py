1d8ad9077b3c5f3cd7f4d7a2a9d8c9f588050d40
subhamghimire <subham.ghimire26@gmail.com>
2019-11-03 10:58:47 +0545

    ok

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "192."
+port = 1255
+
+s.bind((host,port))
+#max 5 clients can be accepted
+s.listen(5)
+#accept connection with client
+socketclient, address = s.accept()
+
+###print("Got Connection from", address)
+
+con = True
+while con:
+    msg = socketclient.recv(1024)
+    msg = msg.decode('utf-8')
+    print(msg)
+    if msg =='quit':
+         s.close()
